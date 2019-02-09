d2d0d43d49a5ec21f6c1e2a6e8d659bbba8eac58
Andres Rivera <andres.rivera.toro@gmail.com>
2019-02-09 06:42:21 -0500

    Add tests for DELETE

--- server.spec.js
+++ server.spec.js
@@ -4,12 +4,12 @@ const server = require('./server.js');
 
 describe('server.js', () => {
   describe('POST', () => {
-    it('returns 201 (Created) status code', async () => {
+    it('returns a 201 (Created) status code', async () => {
       const response = await request(server).post('/api');
       expect(response.statusCode).toBe(201);
     });
 
-    it('returns application/json', async () => {
+    it('returns content-type application/json', async () => {
       const response = await request(server).post('/api');
       expect(response.type).toBe('application/json');
     });
@@ -17,6 +17,24 @@ describe('server.js', () => {
     it('returns a JSON object', async () => {
       const expectedBody = { name: 'Andres' };
       const response = await request(server).post('/api');
+      expect(response.body).toEqual(expectedBody);
+    });
+  });
+
+  describe('DELETE', () => {
+    it('returns a 200 (OK) status code', async () => {
+      const response = await request(server).delete('/api');
+      expect(response.statusCode).toBe(200);
+    });
+
+    it('returns content-type application/json', async () => {
+      const response = await request(server).delete('/api');
+      expect(response.type).toBe('application/json');
+    });
+
+    it('returns a JSON object', async () => {
+      const expectedBody = { message: 'deleted' };
+      const response = await request(server).delete('/api');
       expect(response.body).toEqual(expectedBody);
     });
   });
